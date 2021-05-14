f7b9cbeab6824456d7aba6949faa1fe7713d3b82
Maciek <maciej.oles@gmail.com>
2021-05-14 23:07:22 +0200

    Udoskonalenie Observera

--- app/src/main/java/com/example/reservationtabledesignpatterns/onChange.java
+++ app/src/main/java/com/example/reservationtabledesignpatterns/onChange.java
@@ -2,7 +2,6 @@ package com.example.reservationtabledesignpatterns;
 
 public class onChange extends DaneOsoboweDesignPatternsActivity{
     public Boolean zmiana(Boolean flaga){
-        flaga=true;
-        return flaga;
+        return true;
     }
 }
